6a024eea1e8c08eec9566e241fc9fc31e7d86263
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:08:23 +0530

    Create SumInteger.java

---
+++ LanguageBasics/src/SumInteger.java
@@ -0,0 +1,13 @@
+// 3
+
+public class SumInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
+
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+	}
+
+}
