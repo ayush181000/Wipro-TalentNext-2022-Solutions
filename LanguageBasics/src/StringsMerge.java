c3988c5c773f6018c1cdcb54a462b96dfc72a381
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:08:21 +0530

    Create StringsMerge.java

---
+++ LanguageBasics/src/StringsMerge.java
@@ -0,0 +1,11 @@
+// 1
+public class StringsMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println(args[0]+" Technologies " + args[1]);
+
+	}
+
+}
