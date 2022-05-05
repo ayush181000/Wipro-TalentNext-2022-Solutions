5f70ba47b6008da77e030d51da286f31ec834376
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:28:20 +0530

    Create AsciiArr.java

---
+++ Arrays/src/AsciiArr.java
@@ -0,0 +1,17 @@
+// 4
+
+public class AsciiArr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 97, 2, 5, 7, 89, 43, 12, 67, 9, 56, 23, 6, 234, 7, 678 };
+
+		for (int i = 0; i < arr.length; i++) {
+			char ch = (char) arr[i];
+			System.out.print(ch);
+		}
+
+	}
+
+}
