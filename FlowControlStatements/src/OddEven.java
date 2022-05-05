7ba025dda762e3f1fd8a35fbe3b606ac66b8ebee
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:07:54 +0530

    Create OddEven.java

---
+++ FlowControlStatements/src/OddEven.java
@@ -0,0 +1,16 @@
+// 2
+
+import java.util.Scanner;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		if (sc.nextInt() % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+	}
+}
