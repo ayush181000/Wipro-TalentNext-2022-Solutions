c68821bd5c7b60c4e08c6e0d7047aafa589a71b8
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:08:03 +0530

    Create PrintEven.java

---
+++ FlowControlStatements/src/PrintEven.java
@@ -0,0 +1,14 @@
+// 11
+
+public class PrintEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 23; i <= 57; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
