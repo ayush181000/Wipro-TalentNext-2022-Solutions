00cb8767c8891a900cb650642615b9e22bfe0483
Ayush Garg <ayush181000@gmail.com>
2022-05-06 04:08:28 +0530

    Create FloydsPyramid.java

---
+++ FlowControlStatements/src/FloydsPyramid.java
@@ -0,0 +1,18 @@
+// 15
+
+public class FloydsPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
