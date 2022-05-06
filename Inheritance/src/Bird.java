727be93443f388efdd2670067c546c516f1e3a5f
Ayush Garg <ayush181000@gmail.com>
2022-05-06 07:43:23 +0530

    Create Bird.java

---
+++ Inheritance/src/Bird.java
@@ -0,0 +1,19 @@
+// 1 b
+
+public class Bird extends Animal {
+
+	@Override
+	public void eat() {
+		System.out.println("Bird is eating");
+	}
+
+	@Override
+	public void sleep() {
+		System.out.println("Bird is sleeping");
+	}
+
+	public void fly() {
+		System.out.println("Bird is flying");
+	}
+
+}
