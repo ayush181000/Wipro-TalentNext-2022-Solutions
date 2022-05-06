3824df9ed396c5b063baf4f210b696628a651902
Ayush Garg <ayush181000@gmail.com>
2022-05-06 07:43:21 +0530

    Create Test.java

---
+++ Inheritance/src/Test.java
@@ -0,0 +1,15 @@
+// 1 c
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bird bird = new Bird();
+		bird.eat();
+		bird.sleep();
+		bird.fly();
+
+	}
+
+}
