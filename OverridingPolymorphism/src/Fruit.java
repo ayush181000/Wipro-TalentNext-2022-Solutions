6467edec47acc63e18aeb57a44303e15903ed8ca
Ayush Garg <ayush181000@gmail.com>
2022-05-06 08:36:33 +0530

    Create Fruit.java

---
+++ OverridingPolymorphism/src/Fruit.java
@@ -0,0 +1,13 @@
+// 1 a
+
+public class Fruit {
+
+	private String name;
+	private String taste;
+	private String color;
+
+	public void eat() {
+		System.out.println("The " + name + " tastes " + taste);
+	}
+
+}
