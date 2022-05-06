c73ad2b6d3a309c0a67f8e2198c838fbf84fb813
Ayush Garg <ayush181000@gmail.com>
2022-05-06 07:43:56 +0530

    Create Author.java

---
+++ EncapsulationAbstraction/src/Author.java
@@ -0,0 +1,39 @@
+// 1
+
+public class Author {
+
+	private String name;
+	private String email;
+	private char gender;
+
+	public Author(String name, String email, char gender) {
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+}
