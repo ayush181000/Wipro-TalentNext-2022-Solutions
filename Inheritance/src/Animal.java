182969bfab1158a35775a27834033c39b9b59bcf
Ayush Garg <ayush181000@gmail.com>
2022-05-06 07:43:27 +0530

    Create Animal.java

---
+++ Inheritance/src/Animal.java
@@ -0,0 +1,13 @@
+// 1 a
+
+public class Animal {
+
+	public void eat() {
+		System.out.println("Animal is eating");
+	}
+
+	public void sleep() {
+		System.out.println("Animal is sleeeping");
+	}
+
+}
