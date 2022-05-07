8ca48eddf08df7ef932f553c140d48c201056f06
Ayush Garg <ayush181000@gmail.com>
2022-05-08 04:06:46 +0530

    Create Veena.java

---
+++ Interfaces/src/music/string/Veena.java
@@ -0,0 +1,15 @@
+// 1 b
+
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Veena is being played");
+	}
+
+}
