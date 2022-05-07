148507d324766c6807c5c1f09b28744436f0b7bd
Ayush Garg <ayush181000@gmail.com>
2022-05-07 09:22:29 +0530

    Create Foundation.java

---
+++ Packages/src/testPackage/Foundation.java
@@ -0,0 +1,19 @@
+// 1 a
+
+package testPackage;
+
+public class Foundation {
+
+	// private
+	private int var1;
+
+	// default
+	int var2;
+
+	// public
+	public int var3;
+
+	// protected
+	protected int var4;
+
+}
