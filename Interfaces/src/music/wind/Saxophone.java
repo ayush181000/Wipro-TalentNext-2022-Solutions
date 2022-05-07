51c0280a3c7e37d5fcb8a03de29df3848a4102cc
Ayush Garg <ayush181000@gmail.com>
2022-05-08 04:06:44 +0530

    Create Saxophone.java

---
+++ Interfaces/src/music/wind/Saxophone.java
@@ -0,0 +1,15 @@
+// 1 c
+
+package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Saxophone is being played");
+	}
+
+}
