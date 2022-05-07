59e61ff6b95a8371b1460a2ef6f825e247b35556
Ayush Garg <ayush181000@gmail.com>
2022-05-07 10:10:52 +0530

    Create Vehicle.java

---
+++ Packages/src/com/automobile/Vehicle.java
@@ -0,0 +1,13 @@
+// 3 a
+
+package com.automobile;
+
+public abstract class Vehicle {
+
+	public abstract String getModelName();
+
+	public abstract String getRegistrationNumber();
+
+	public abstract String getOwnerName();
+
+}
