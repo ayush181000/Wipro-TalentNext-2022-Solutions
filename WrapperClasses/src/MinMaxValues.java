bbc4daea90f90f938e3efe88187031fdf7c1d6c8
Ayush Garg <ayush181000@gmail.com>
2022-05-08 06:31:52 +0530

    .

---
+++ WrapperClasses/src/MinMaxValues.java
@@ -0,0 +1,22 @@
+// 1
+
+public class MinMaxValues {
+
+	public static void main(String[] args) {
+
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+
+	}
+
+}
