36927d1958667d9126ea9fe1466a202a516c41d1
Ayush Garg <ayush181000@gmail.com>
2022-05-08 18:24:57 +0530

    Create Assignment1LL.java

---
+++ List/src/Assignment1LL.java
@@ -0,0 +1,30 @@
+// 5
+
+import java.util.LinkedList;
+
+public class Assignment1LL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> list = new LinkedList<>();
+
+		list.add("jan");
+		list.add("feb");
+		list.add("mar");
+		list.add("apr");
+		list.add("may");
+		list.add("jun");
+		list.add("jul");
+		list.add("aug");
+		list.add("sep");
+		list.add("oct");
+		list.add("nov");
+		list.add("dec");
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+	}
+
+}
