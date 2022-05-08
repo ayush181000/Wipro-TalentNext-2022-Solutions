4d27e8b592b6a5e9a5094930da6c337a41b939f5
Ayush Garg <ayush181000@gmail.com>
2022-05-08 07:11:01 +0530

    Create Employee.java

---
+++ WrapperClasses/src/Employee.java
@@ -0,0 +1,19 @@
+// 4 a
+
+public class Employee {
+
+	private String name;
+
+	public Employee(String name) {
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
