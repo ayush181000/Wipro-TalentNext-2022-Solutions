7a2b12708d182b15111361814774e3d3ed07da9c
Ayush Garg <ayush181000@gmail.com>
2022-05-08 06:39:29 +0530

    Create intToOctaHex.java

---
+++ WrapperClasses/src/intToOctaHex.java
@@ -0,0 +1,16 @@
+// 2 
+
+public class intToOctaHex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 100;
+		
+		System.out.println(Integer.toHexString(x));
+		System.out.println(Integer.toBinaryString(x));
+		System.out.println(Integer.toOctalString(x));
+
+	}
+
+}
