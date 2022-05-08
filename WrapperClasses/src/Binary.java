8d4964fc213f752c7da9873358b5cc2231889bc5
Ayush Garg <ayush181000@gmail.com>
2022-05-08 07:10:59 +0530

    Create Binary.java

---
+++ WrapperClasses/src/Binary.java
@@ -0,0 +1,12 @@
+// 3
+
+public class Binary {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		
+		System.out.println(String.format("%08d", Integer.parseInt(Integer.toBinaryString(x))));
+	}
+	
+}
