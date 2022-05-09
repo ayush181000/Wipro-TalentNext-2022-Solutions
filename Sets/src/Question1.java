ca592ba444453742ef65f64a6534f82cb32e125c
Ayush Garg <ayush181000@gmail.com>
2022-05-09 09:14:24 +0530

    .

--- Sets/src/Question1.java
+++ Sets/src/Question1.java
@@ -1,9 +1,39 @@
+// 1
+
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Question1 {
 
+	static HashSet<String> H1 = new HashSet<>();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		saveCountry("India");
+		saveCountry("Tokyo");
+		saveCountry("Australia");
+		saveCountry("China");
 
+		System.out.println(getCountry("India"));
+		System.out.println(getCountry("Indi"));
+
+	}
+
+	public static void saveCountry(String country) {
+		H1.add(country);
+	}
+
+	public static String getCountry(String country) {
+		Iterator<String> it = H1.iterator();
+
+		while (it.hasNext()) {
+			String next = it.next();
+			if (next == country) {
+				return next;
+			}
+		}
+
+		return null;
 	}
 
 }
